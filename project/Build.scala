884cd3b401d4a4d70c3a02098799929b8af2aeed
Philipp <philipp.ossler@gmail.com>
2013-05-07 22:31:57 +0200

    Vorbereitung für Deployment nach Heroku

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,9 @@ object ApplicationBuild extends Build {
   val appDependencies = Seq(
     // Add your project dependencies here,
     jdbc,
-    anorm
+    anorm,
+    
+    "postgresql" % "postgresql" % "8.4-702.jdbc4"
   )
 
 
